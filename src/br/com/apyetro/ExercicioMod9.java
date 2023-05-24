3b2bbc17254ec772cfb7a77b5e40d88fc7173183
Anderson Pyetro <endlessmidia@gmail.com>
2023-05-24 19:38:34 -0300

    Exercicio Mod9

---
+++ src/br/com/apyetro/ExercicioMod9.java
@@ -0,0 +1,17 @@
+package br.com.apyetro;
+
+public class ExercicioMod9 {
+
+    public static void main(String args[]){
+
+        int num1 = 12341234;
+        System.out.println(num1);
+        Integer num2 = num1;
+        System.out.println(num2);
+
+        long numI = 123414454452l;
+        System.out.println(numI);
+        int numJ = (int) numI;
+        System.out.println(numJ);
+    }
+}
